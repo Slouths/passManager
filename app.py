9d2a7ea719276fefbc0b7f404d7431f2919387dc
Slouths <nicholasnun06@gmail.com>
2023-10-13 19:11:28 -0400

    Added comments for clear code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-
+#Function that generates a password based on the parameters.
 def generate_password(length, numbers = True, special = True):
     characters = string.ascii_letters
     digits = string.digits
@@ -9,22 +9,25 @@ def generate_password(length, numbers = True, special = True):
 
     return special
 
-
+#Asks the user if they want to replay the password program
 def replay():
     run = input("Do you want to run the program again? ")
     if run == ["y", "yes"]:
         return True
     return False
 
+#Main runs until user does not want to generate anymore passwords
 while True:
     print("Welcome to your password generator!")
 
+    #Tries to turn length into an int, if it cant then the user is prompted again
     try:
         length = int(input("How many characters do you want your password to be? "))
     except Exception:
         print("Please enter a valid length")
         length = int(input("How many characters do you want your password to be? "))
 
+    #Gets number option
     nums = input("Do you want your password to include numbers? Y/N ").lower()
 
     if nums == ["y", "yes", "ye"]:
@@ -35,7 +38,7 @@ while True:
         print("Please enter a valid letter (Y / N)")
         nums = input("Do you want your password to include numbers? ").lower()
     
-
+    #Gets special character option
     special = input("Do you want your password to include special characters? Y/N ").lower()
 
     if nums == ["y", "yes", "ye"]:
@@ -46,8 +49,10 @@ while True:
         print("Please enter a valid letter (Y / N)")
         special = input("Do you want your password to include special characters? ").lower()
 
+    #Prints out the password
     print(generate_password(length, nums, special))
 
+    #Checks if user wants to generate another password
     if replay():
         continue
     else:
