e5a5c5d2900865cd2724d09da134aa79d34680db
Nicholas Nunez <nicholasnunez@Nicholass-MacBook-Pro-2.local>
2023-10-13 17:38:44 -0400

    Added basic password funcionality

--- app.py
+++ app.py
@@ -1,5 +1,59 @@
-def addPassword():
-    pass
+import random
+import string
 
-def getPassword():
-    pass
+
+def generate_password(length, numbers = True, special = True):
+    characters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    return special
+
+
+def replay():
+    run = input("Do you want to run the program again? ")
+    if run == ["y", "yes"]:
+        return True
+    return False
+
+while True:
+    print("Welcome to your password generator!")
+
+    try:
+        length = int(input("How many characters do you want your password to be? "))
+    except Exception:
+        print("Please enter a valid length")
+        length = int(input("How many characters do you want your password to be? "))
+
+    nums = input("Do you want your password to include numbers? Y/N ").lower()
+
+    if nums == ["y", "yes", "ye"]:
+        nums = True
+    elif nums == ["n", "no", "nah"]:
+        nums = False
+    else:
+        print("Please enter a valid letter (Y / N)")
+        nums = input("Do you want your password to include numbers? ").lower()
+    
+
+    special = input("Do you want your password to include special characters? Y/N ").lower()
+
+    if nums == ["y", "yes", "ye"]:
+        nums = True
+    elif nums == ["n", "no", "nah"]:
+        nums = False
+    else:
+        print("Please enter a valid letter (Y / N)")
+        special = input("Do you want your password to include special characters? ").lower()
+
+    print(generate_password(length, nums, special))
+
+    if replay():
+        continue
+    else:
+        break
+    
+    
+    
+    
+
